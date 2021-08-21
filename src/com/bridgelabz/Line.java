a7bc1bd68fa7f84ab1c8921c1bdff91cf312b779
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-21 18:20:29 +0530

    MODIFY: Implement Euclidean distance

--- src/com/bridgelabz/Line.java
+++ src/com/bridgelabz/Line.java
@@ -1,7 +1,40 @@
 package com.bridgelabz;
 
 public class Line {
+	double x1, y1, x2, y2;
+
+	class Point {
+		double x;
+		double y;
+
+		public Point(double x, double y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	Point p1, p2;
+
+	public Line(double x1, double y1, double x2, double y2) {
+		this.p1 = new Point(x1, y1);
+		this.p2 = new Point(x2, y2);
+	}
+
+	public double getLength() {
+		return Math.round(Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)) * 1000000d) / 1000000d;
+	}
+
+	
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program");
-}
+		Line L1 = new Line(-7, -4, 17, 6.5);
+		
+		System.out.println("Welcome to Line Comparison Computation Program\n");
+		
+		// Use case - 1
+		System.out.println("Length of Line 1 = " + L1.getLength());
+		System.out.println();
+		
+		
+	}
 }
